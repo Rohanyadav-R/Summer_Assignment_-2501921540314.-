1c168bbc0341a47eb69c9babc5153c9b99fc4e5f
Rohanyadav-R <rohanglobal56@gmail.com>
2026-06-08 13:05:39 +0530

    Add files via upload

---
+++ Day1/MultiplicationTable.c
@@ -0,0 +1,18 @@
+
+#include <stdio.h>
+
+int main()
+{
+    int n,i,product;
+    printf("Enter the number :");
+    scanf("%d",&n);
+    for(i=1;i<=10; i++){
+        product =n*i;
+        printf("%d*%d = %d\n",n,i,product);
+        
+        
+    }
+    
+
+    return 0;
+}
