31564620600e3107b5de80ef4e668f710fe35c58
Rohanyadav-R <rohanglobal56@gmail.com>
2026-06-09 15:51:05 +0530

    Add files via upload

---
+++ Day15/Zeroend.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main(){
+    int n;
+    printf("Enter the number : ");
+    scanf("%d",&n);
+    int arr[n];
+    printf("Enter the all element of array : ");
+    for(int i=0;i<n;i++){
+    scanf("%d",&arr[i]);
+    }
+    for(int i=0;i<n;i++){
+        for(int j=i+1;j<n;j++){
+            if(arr[i] == 0){
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+            }
+        }
+    }
+for(int i=0;i<n;i++){
+    printf("%d ",arr[i]);
+}
+    return 0;
+}
