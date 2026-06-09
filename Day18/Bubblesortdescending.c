d14d4a5e261235720ac0068dfd1a919ee40474c6
Rohanyadav-R <rohanglobal56@gmail.com>
2026-06-09 15:59:01 +0530

    Add files via upload

---
+++ Day18/Bubblesortdescending.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+#include<stdbool.h>
+int main(){
+    int n;
+    printf("Enter the size of array :");
+    scanf("%d",&n);
+    int arr[n];
+    printf("Enter the element of unsorted array :\n");
+    for(int i=0;i<n;i++){
+        scanf("%d",&arr[i]);
+    }
+    for(int i=0;i<n-1;i++){
+        bool flag = true;
+        for(int j=0;j<n-1-i;j++){
+            if(arr[j]<arr[j+1]){
+                int temp =arr[j];
+                arr[j]=arr[j+1];
+                arr[j+1]=temp;
+                flag =false;
+            }
+        }
+        if(flag==true) break;
+    }
+    printf("The sorted array is :\n");
+    for(int i=0;i<n;i++){
+        printf("%d ",arr[i]);
+    }
+    return 0;
+    
+}
