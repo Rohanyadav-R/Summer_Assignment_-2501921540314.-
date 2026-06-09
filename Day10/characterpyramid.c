6a8c0452175e13afcd9a4503ee50d5ddb4096741
Rohanyadav-R <rohanglobal56@gmail.com>
2026-06-09 15:32:05 +0530

    Add files via upload

---
+++ Day10/characterpyramid.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main() {
+	int i,j,n,k,q;
+	printf("Enter the number : ");
+	scanf("%d",&n);
+	for( int i=1; i<=n; i++) {
+		for( j=1; j<=n-i; j++) {
+		    printf(" ");
+		}
+	for(int k=1;k<=i;k++){
+	    printf("%c",64+k);
+	}
+	int a=i-1;
+	for(int q=1;q<=i-1;q++){
+	    printf("%c",64+a);
+	    a--;
+	}
+		printf("\n");
+	}
+	return 0;
+}
